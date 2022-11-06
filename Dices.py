cb9aa1b8a7aba118803212ef6ea0fa6ab0a0daf2
Bogyi95 <krzysiek.bogusz@onet.pl>
2022-11-06 17:54:22 +0100

    Create Dices.py

---
+++ Dices.py
@@ -0,0 +1,18 @@
+#Simple Dices Simulator
+import random
+
+min_value = 1
+max_value = 6
+
+roll = "yes"
+
+while roll == "yes" or roll == "y":
+
+    print("Rolling the dice")
+    print("You rolled :")
+    print(random.randint(min_value, max_value))
+    print("and")
+    print(random.randint(min_value, max_value))
+    print("Do you want to roll again?")
+
+    roll = input("Type y or n : ")
